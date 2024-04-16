4d2fba61556f2fd211e5074f3ce9706af4527626
chenHX <1575197604@qq.com>
2024-04-16 13:53:36 +0800

    fix

--- sk_esd/sk_esd.R
+++ sk_esd/sk_esd.R
@@ -2,9 +2,9 @@ library(ScottKnottESD)
 library(readxl)
 # 数据文件夹路径
 # TODO: 所有路径统一采用绝对路径 间隔符采用 \\ 同时 末尾保留 \\
-data_path <- "E:\\code\\code_back\\python_project\\ac_test_on_mlcq\\record\\AQ2\\single_classifier_record\\"
+data_path <- "E:\\code\\code_back\\python_project\\data_picture_tool\\sk_esd\\test_data\\"
 # esd 离群检测法结果存放路径
-sk_esd_result_path <- 'E:\\code\\code_back\\python_project\\ac_test_on_mlcq\\record\\AQ2\\esd_result\\'
+sk_esd_result_path <- 'E:\\code\\code_back\\python_project\\data_picture_tool\\sk_esd\\output\\'
 # 获取所有csv文件名称
 file_list <- list.files(data_path)
 
@@ -27,24 +27,3 @@ for (file in file_list){
   print("data parsed successed")
 }
 print("all finished")
-
-# for (file in file_list){
-#   print(file)
-#   # print(file)
-#   # # 拼接绝对路径
-#   # path <- paste0(data_path,file)
-#   # print(path)
-#   # # 读取csv文件
-#   # csv <- read_excel(path)
-#   # # 删除第一列
-#   # csv <- csv[,-1]
-#   # # 进行sk_esd分析
-#   # sk = sk_esd(csv)
-#   # splited_str <- gsub(".csv","",file)
-#   # print(splited_str)
-#   # result_path <- paste0(sk_esd_result_path,splited_str,".txt")
-#   # print(result_path)
-#   # write.table(sk[["groups"]], file = result_path)
-#   # print("data parsed successed")
-# }
-# print("all finished")
--- sk_esd_ranking/sk_esd_ranking.R
+++ sk_esd_ranking/sk_esd_ranking.R
@@ -1,24 +1,26 @@
 library(ScottKnottESD)
 library(readxl)
-# TODO: 所有路径统一采用绝对路径 间隔符采用 \\ 同时 末尾保留 \\
-data_path <- "E:\\code\\code_back\\python_project\\ac_test_on_mlcq\\record\\AQ1\\"
-sk_esd_ranking_result_path <- 'E:\\code\\code_back\\python_project\\ac_test_on_mlcq\\record\\AQ1\\esd_ranking_picture\\'
+
+data_path <- "E:\\code\\code_back\\python_project\\data_picture_tool\\sk_esd_ranking\\test_data\\"
+sk_esd_ranking_result_path <- 'E:\\code\\code_back\\python_project\\data_picture_tool\\sk_esd_ranking\\picture\\'
 file_names <- list.files(data_path)
 for(file in file_names){
   if(grepl(pattern = ".xlsx$",x=file)){
     # 获取文件名
-    file_name <- strsplit(file,split = ".xlsx")[[1]][1]
-    print(file_name)
+    parts <- strsplit(file,split = "#")[[1]]
+    dataset <- parts[1]
+    metric <- sub("\\.xlsx","",parts[2])
+
     # 凭借文件路径
     file_path <- paste0(data_path,file)
     print(file_path)
     data <- read_excel(file_path)
     data <- data[,-1]
     sk <- sk_esd(data)
-    picutre_file_name <- paste0('RQ1_Ranking_',file_name,".jpg")
+    picutre_file_name <- paste0('RQ2_Ranking_',dataset,"-",metric,".jpg")
     picutre_file_path <- paste0(sk_esd_ranking_result_path,picutre_file_name)
     print(picutre_file_path)
-    jpeg(file=picutre_file_path,width =2600,height =1200,units = 'px',res = 300)
+    jpeg(file=picutre_file_path,width =5000,height =2000,units = 'px',res = 600)
     plot(sk,
          mar=c(0,0,0,0),
          las=2,
@@ -35,6 +37,5 @@ for(file in file_names){
          ylim  = c(0.2,0.6)
         )
       dev.off()
-
   }
 }
